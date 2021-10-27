95752d94ed5430078cb07d2ec76edf34bc2564ca
leftyxiv <manny@aledoux.net>
2021-10-27 10:44:42 -0400

    webpack in and working, express server serves it up statically

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${ port }`)
+});
