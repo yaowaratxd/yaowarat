5d40f9be5cf0a28df29e0415f83a63dde30edb01
leftyxiv <manny@aledoux.net>
2021-10-28 12:05:25 -0400

    standard index is in

--- client/src/jest.config.js
+++ client/src/jest.config.js
@@ -1,6 +1,8 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
+// boilerplate settings from https://www.freecodecamp.org/news/how-to-set-up-jest-enzyme-like-a-boss-8455a2bc6d56/
+
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
