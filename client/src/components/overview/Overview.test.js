16bb04ff861eba3dc3b3b2cee190e3ec478e39d6
leftyxiv <manny@aledoux.net>
2021-11-09 12:08:09 -0500

    got my test to pass haha

--- client/src/components/overview/Overview.test.js
+++ client/src/components/overview/Overview.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import sampleData from '../../sampleData.json'
+
 
 configure({ adapter: new Adapter() });
 
@@ -9,7 +11,7 @@ import Overview from './Overview.jsx';
 
 describe('Overview component', () => {
   it ('Should render to the screen', () => {
-    const component = shallow(<Overview />);
+    const component = shallow(<Overview product={sampleData[0]}/>);
     expect(component).toBeTruthy();
   });
 });
